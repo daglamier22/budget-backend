89e529a99c27b1db4e1f0fcdb550b547bf45e477
Brian McCarthy <daglamier22@gmail.com>
2019-01-17 17:13:00 -0600

    Hello World Test

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/helloworld', (req, res, next) => {
+  res.json('Hello World!');
+});
+
+app.listen(3000);
