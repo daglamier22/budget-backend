a807cae951d6b7b68951d4b236acf1dcd138971b
Brian McCarthy <daglamier22@gmail.com>
2019-02-09 14:18:38 -0600

    change version url and remove auth requirement

--- routes/tests.js
+++ routes/tests.js
@@ -1,12 +1,9 @@
 const express = require('express');
 
 const testController = require('../controllers/tests');
-const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 router.get('/version', testController.getVersion);
-
-router.get('/version', isAuth, testController.getVersion);
 
 module.exports = router;
