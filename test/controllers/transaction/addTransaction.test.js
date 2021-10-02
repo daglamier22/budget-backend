4cf95fc2bc37f9ac7a4d2d494d54035c24d7ae1f
Brian McCarthy <daglamier22@gmail.com>
2021-10-02 15:25:09 -0500

    create unit tests for transaction controllers

---
+++ test/controllers/transaction/addTransaction.test.js
@@ -0,0 +1,133 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.CI ?
+  'mongodb://localhost' :
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DEFAULT_DATABASE}-test?retryWrites=true`;
+
+const Transaction = require('../../../models/transaction');
+const { addTransaction } = require('../../../controllers/transaction/addTransaction');
+
+describe('Transaction Controller - Add Transaction', function() {
+  let res;
+
+  before(function(done) {
+    mongoose
+      .connect(MONGODB_URI)
+      .then(result => {
+        const transaction = new Transaction({
+          _id: '6c0f66b979af55031b34728a',
+          userId: '5c0f66b979af55031b34728a',
+          date: '02-25-2019',
+          accountName: '5c0f66b979af55031b34728a',
+          description: 'description',
+          categoryParent: 'categoryParent',
+          categoryChild: 'categoryChild',
+          amount: '10.00',
+          transactionType: 'debit',
+          note: ''
+        });
+        return transaction.save();
+      })
+      .then(() => {
+        done();
+      })
+      .catch(err => {
+        done(err);
+      });
+  });
+
+  beforeEach(function() {
+    res = {
+      statusCode: null,
+      apiStatus: null,
+      apiMessage: null,
+      errorCode: null,
+      status: function(code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function(data) {
+        this.apiStatus = data.apiStatus;
+        this.apiMessage = data.apiMessage;
+        this.errorCode = data.errorCode;
+        this.values = data.values;
+      }
+    };
+  });
+
+  it('should return an error if saving to the database fails', function(done) {
+    sinon.stub(Transaction.prototype, 'save');
+    Transaction.prototype.save.throws();
+
+    const req = {
+      userId: '5c0f66b979af55031b34728a',
+      body: {
+        date: '02-25-2019',
+        accountName: '5c0f66b979af55031b34728a',
+        description: 'new description',
+        categoryParent: 'new categoryParent',
+        categoryChild: 'new categoryChild',
+        amount: '20.0',
+        transactionType: 'debit',
+        note: 'test'
+      }
+    };
+    addTransaction(req, res, () => {}).then(() => {
+      expect(res).to.have.property('statusCode');
+      expect(res.statusCode).to.equal(500);
+      expect(res).to.have.property('apiStatus');
+      expect(res.apiStatus).to.be.equal('FAILURE');
+      expect(res).to.have.property('apiMessage');
+      expect(res.apiMessage).to.be.equal('Unable to create transaction');
+      expect(res).to.have.property('errorCode');
+      expect(res.errorCode).to.be.equal(2);
+      done();
+    }).catch(err => {
+      done(err);
+    });
+
+    Transaction.prototype.save.restore();
+  });
+
+  it('should return success if transaction created', function(done) {
+    const req = {
+      userId: '5c0f66b979af55031b34728a',
+      body: {
+        date: '02-25-2019',
+        accountNameId: '5c0f66b979af55031b34728a',
+        description: 'new description',
+        categoryParent: 'new categoryParent',
+        categoryChild: 'new categoryChild',
+        amount: '20.0',
+        transactionType: 'debit',
+        note: 'test'
+      }
+    };
+    addTransaction(req, res, () => {}).then(() => {
+      expect(res).to.have.property('statusCode');
+      expect(res.statusCode).to.be.equal(201);
+      expect(res).to.have.property('apiStatus');
+      expect(res.apiStatus).to.equal('SUCCESS');
+      expect(res).to.have.property('apiMessage');
+      expect(res.apiMessage).to.equal('Transaction created');
+      expect(res).to.have.property('errorCode');
+      expect(res.errorCode).to.equal(0);
+      done();
+    }).catch(err => {
+      done(err);
+    });
+  });
+
+  after(function(done) {
+    Transaction.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+});
---
+++ test/controllers/transaction/getTransactionsByAccountName.test.js
@@ -0,0 +1,147 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.CI ?
+  'mongodb://localhost' :
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DEFAULT_DATABASE}-test?retryWrites=true`;
+
+const Transaction = require('../../../models/transaction');
+const { getTransactionsByAccountName } = require('../../../controllers/transaction/getTransactionsByAccountName');
+
+describe('Transaction Controller - Get Transactions By AccountName', function() {
+  let res;
+
+  before(function(done) {
+    mongoose
+      .connect(MONGODB_URI)
+      .then(result => {
+        const transaction = new Transaction({
+          _id: '6c0f66b979af55031b34728a',
+          userId: '5c0f66b979af55031b34728a',
+          date: '02-25-2019',
+          accountName: '5c0f66b979af55031b34728a',
+          description: 'description',
+          categoryParent: 'categoryParent',
+          categoryChild: 'categoryChild',
+          amount: '10.00',
+          transactionType: 'debit',
+          note: ''
+        });
+        return transaction.save();
+      })
+      .then(() => {
+        done();
+      })
+      .catch(err => {
+        done(err);
+      });
+  });
+
+  beforeEach(function() {
+    res = {
+      statusCode: null,
+      apiStatus: null,
+      apiMessage: null,
+      errorCode: null,
+      status: function(code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function(data) {
+        this.apiStatus = data.apiStatus;
+        this.apiMessage = data.apiMessage;
+        this.errorCode = data.errorCode;
+        this.values = data.values;
+      }
+    };
+  });
+
+  it('should return an error if retrieving from the database fails', function(done) {
+    sinon.stub(mongoose.Model, 'find');
+    mongoose.Model.find.throws();
+
+    const req = {
+      userId: '5c0f66b979af55031b34728a',
+      params: {
+        accountId: '5c0f66b979af55031b34728a'
+      }
+    };
+    getTransactionsByAccountName(req, res, () => {}).then(() => {
+      expect(res).to.have.property('statusCode');
+      expect(res.statusCode).to.equal(500);
+      expect(res).to.have.property('apiStatus');
+      expect(res.apiStatus).to.be.equal('FAILURE');
+      expect(res).to.have.property('apiMessage');
+      expect(res.apiMessage).to.be.equal('Unable to retrieve transactions');
+      expect(res).to.have.property('errorCode');
+      expect(res.errorCode).to.be.equal(2);
+      done();
+    }).catch(err => {
+      done(err);
+    });
+
+    mongoose.Model.find.restore();
+  });
+
+  it('should return success if transactions retrieved but empty list because account wrong account name was used', function(done) {
+    const req = {
+      userId: '5c0f66b979af55031b34728a',
+      params: {
+        accountId: '5c0f66b979af55031b34728b' // this needs to be different than accountName in begin function
+      }
+    };
+    getTransactionsByAccountName(req, res, () => {}).then(() => {
+      expect(res).to.have.property('statusCode');
+      expect(res.statusCode).to.be.equal(200);
+      expect(res).to.have.property('apiStatus');
+      expect(res.apiStatus).to.equal('SUCCESS');
+      expect(res).to.have.property('apiMessage');
+      expect(res.apiMessage).to.equal('Transactions retrieved');
+      expect(res).to.have.property('errorCode');
+      expect(res.errorCode).to.equal(0);
+      expect(res).to.have.property('values');
+      expect(res.values).to.have.property('transactions');
+      expect(res.values.transactions).to.deep.equal([]);
+      done();
+    }).catch(err => {
+      done(err);
+    });
+  });
+
+  it('should return success if transactions retrieved with 1 transaction', function(done) {
+    const req = {
+      userId: '5c0f66b979af55031b34728a',
+      params: {
+        accountId: '5c0f66b979af55031b34728a'
+      }
+    };
+    getTransactionsByAccountName(req, res, () => {}).then(() => {
+      expect(res).to.have.property('statusCode');
+      expect(res.statusCode).to.be.equal(200);
+      expect(res).to.have.property('apiStatus');
+      expect(res.apiStatus).to.equal('SUCCESS');
+      expect(res).to.have.property('apiMessage');
+      expect(res.apiMessage).to.equal('Transactions retrieved');
+      expect(res).to.have.property('errorCode');
+      expect(res.errorCode).to.equal(0);
+      expect(res).to.have.property('values');
+      expect(res.values).to.have.property('transactions');
+      expect(res.values.transactions).to.have.length(1);
+      done();
+    }).catch(err => {
+      done(err);
+    });
+  });
+
+  after(function(done) {
+    Transaction.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+});
